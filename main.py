8ad3329307ec3515f427f96be841ba23d8dc5558
janhh011 <hoeltkemeier@uni-potsdam.de>
2025-05-25 17:27:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+from util import get_limits
+from PIL import Image
+
+
+yellow = [0,255,255] #red in BGR
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+
+    #translate frame in HSV
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #get lower and upper limit of color yellow
+    lowerlimit, upperlimit = get_limits(color=yellow)
+
+    #create a mask of the frame where only the selected colors are visible
+    mask = cv2.inRange(hsvImage, lowerlimit, upperlimit)
+
+    #convert the array to PILLOW
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+    
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 5)
+
+    cv2.imshow("Frame", frame)
+    if(cv2.waitKey(1)) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ util.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+def get_limits(color : list)->list:
+    c = np.uint8([[color]]) #put BGR value in a 1x1 Image (correct formatting for cv2)
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV) #translate BGR to HSV
+
+
+
+    #cast to int to avoid overflow
+    h=int(hsvC[0][0][0])
+
+    #hsvC[0][0][0] → Hue, hsvC[0][0][1] → Saturation, hsvC[0][0][2] → Value
+    lower_limit = max(h - 10, 0), 100, 100
+    upper_limit = min(hsvC[0][0][0] + 10, 179), 255, 255
+
+    lower_limit = np.array(lower_limit, dtype=np.uint8)
+    upper_limit = np.array(upper_limit, dtype=np.uint8)
+
+    return lower_limit, upper_limit
